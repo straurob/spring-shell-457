93d783b284470f241793676f801e6c4a5425dab7
Robert Strauch <straurob@users.noreply.github.com>
2022-07-13 22:52:15 +0200

    initial commit

---
+++ src/main/java/de/straurob/springshell457/SpringShell457Application.java
@@ -0,0 +1,13 @@
+package de.straurob.springshell457;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShell457Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShell457Application.class, args);
+    }
+
+}
---
+++ src/test/java/de/straurob/springshell457/SpringShell457ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.straurob.springshell457;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShell457ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
